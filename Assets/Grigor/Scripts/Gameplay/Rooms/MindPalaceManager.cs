182388926139d2353bd27b92909136318bfe1a8e
RazerHS <igor.kovacevic550@gmail.com>
2023-10-09 10:31:51 +0200

    mind palace widget + room change wip

---
+++ Assets/Grigor/Scripts/Gameplay/Rooms/MindPalaceManager.cs
@@ -0,0 +1,44 @@
+﻿using CardboardCore.DI;
+using Grigor.Characters;
+using Grigor.Characters.Components;
+using Grigor.Characters.Components.Player;
+using UnityEngine;
+
+namespace Grigor.Gameplay.Rooms
+{
+    [Injectable]
+    public class MindPalaceManager : CardboardCoreBehaviour
+    {
+        [Inject] private RoomRegistry roomRegistry;
+        [Inject] private CharacterRegistry characterRegistry;
+
+        private Vector3 previousPositionInMindPalace;
+        private Vector3 previousPositionInOverworld;
+
+        private Player player;
+        private Room mindPalaceRoom;
+
+        protected override void OnInjected()
+        {
+            player = characterRegistry.Player;
+            mindPalaceRoom = roomRegistry.GetRoom(RoomNames.MindPalace);
+        }
+
+        protected override void OnReleased()
+        {
+
+        }
+
+        public void EnterMindPalace(Vector3 overworldPosition)
+        {
+            if (previousPositionInMindPalace == Vector3.zero)
+            {
+                previousPositionInMindPalace = mindPalaceRoom.SpawnPoint.SpawnPosition;
+            }
+
+
+
+            previousPositionInOverworld = overworldPosition;
+        }
+    }
+}
---
+++ Assets/Grigor/Scripts/UI/Widgets/ToggleMindPalaceWidget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Grigor.UI.Widgets
+{
+    public class ToggleMindPalaceWidget : UIWidget
+    {
+        [SerializeField] private Button button;
+
+        protected override void OnShow()
+        {
+            button.onClick.AddListener(OnToggleMindPalaceClicked);
+        }
+
+        protected override void OnHide()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnToggleMindPalaceClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
