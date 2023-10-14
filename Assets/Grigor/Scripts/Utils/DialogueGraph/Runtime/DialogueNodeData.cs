558c531abfb61761ca4e181e2522369cd7c528f0
RazerHS <igor.kovacevic550@gmail.com>
2023-10-15 00:40:29 +0200

    dialogue graph iteration

---
+++ Assets/Grigor/Scripts/Utils/DialogueGraph/Runtime/DialogueNodeData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace RazerCore.Utils.DialogueGraph.Runtime
+{
+    [Serializable]
+    public class DialogueNodeData
+    {
+        public string NodeGUID;
+        public string DialogueText;
+        public Vector2 Position;
+    }
+}
---
+++ Assets/Grigor/Scripts/Utils/DialogueGraph/Runtime/NodeLinkData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RazerCore.Utils.DialogueGraph.Runtime
+{
+    [Serializable]
+    public class NodeLinkData
+    {
+        public string BaseNodeGUID;
+        public string PortName;
+        public string TargetNodeGUID;
+    }
+}
