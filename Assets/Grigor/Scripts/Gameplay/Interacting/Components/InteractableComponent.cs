2b884789ca73525c09809ef89c40d0e4c3862708
RazerHS <igor.kovacevic550@gmail.com>
2023-12-10 21:34:27 +0100

    adding tooltips to interactable component fields

--- Assets/Grigor/Scripts/Gameplay/Interacting/Components/InteractableComponent.cs
+++ Assets/Grigor/Scripts/Gameplay/Interacting/Components/InteractableComponent.cs
@@ -11,14 +11,24 @@ namespace Grigor.Gameplay.Interacting.Components
 {
     public class InteractableComponent : MonoBehaviour
     {
+        [PropertyTooltip("Should this component be triggered when the player is in range?")]
         [SerializeField, ColoredBoxGroup("Config", false, true)] protected bool interactInRange;
 
+        [PropertyTooltip("Will this component remove itself from the chain so that it cannot be interacted with again after the first interaction?")]
         [SerializeField, ColoredBoxGroup("Chain", false, true), InfoBox("Stays in chain and also stops the chain!", InfoMessageType.Warning, nameof(CheckConfig))] protected bool removeFromChainAfterEffect;
+
+        [PropertyTooltip("Should this interaction stop the interaction chain and let the player roam?")]
         [SerializeField, ColoredBoxGroup("Chain"), HideIf(nameof(IsOnlyInteractableInChain))] protected bool stopsChain;
+
+        [PropertyTooltip("The element index of this component in the parent interactable chain. You can change this index by clicking the arrows to the right of each element in the chain.")]
         [SerializeField, ColoredBoxGroup("Chain"), HideIf(nameof(IsOnlyInteractableInChain)), ReadOnly] protected int indexInChain;
 
+        [PropertyTooltip("Does this component have an effect when the time of day changes to day and night?")]
         [SerializeField, ColoredBoxGroup("Time", false, true)] protected bool hasTimeEffect;
+
+        [PropertyTooltip("Should time pass after this interaction triggers?")]
         [SerializeField, ColoredBoxGroup("Time")] protected bool timePassesOnInteract;
+
         [SerializeField, ColoredBoxGroup("Time"), ShowIf(nameof(timePassesOnInteract)), Range(0, 60)] protected int minutesToPass = 1;
         [SerializeField, ColoredBoxGroup("Time"), ShowIf(nameof(timePassesOnInteract)), Range(0, 24)] protected int hoursToPass = 1;
 
